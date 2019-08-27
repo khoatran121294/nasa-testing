a14616c3613d03c4a60c23dd6de4ae9eac178123
Khoa Tran <khoatran121294@gmail.com>
2019-08-27 15:49:44 +0700

    fix router on production

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./screens/App";
 import { store } from "./store";
@@ -9,8 +9,9 @@ import { store } from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
+    <Router>
       <App />
+    </Router>
   </Provider>,
-
   document.getElementById("root")
 );
--- src/screens/app.js
+++ src/screens/app.js
@@ -1,24 +1,19 @@
 import React, { Component } from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./home";
 import NasaSearch from "./nasa-search";
-// import { createBrowserHistory } from "history";
 import "./app.scss";
-
-// const history = createBrowserHistory();
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <BrowserRouter basename="/">
-          <Switch>
-            <Route exact path="/" component={() => <Home />} />
-            <Route path="/home" component={() => <Home />} />
-            <Route path="/nasa-search" component={() => <NasaSearch />} />
-            <Route render={() => <div className="NotFoundMessage">404 Page Not Found</div>} />
-          </Switch>
-        </BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={() => <Home />} />
+          <Route path="/home" component={() => <Home />} />
+          <Route path="/nasa-search" component={() => <NasaSearch />} />
+          <Route render={() => <div className="NotFoundMessage">404 Page Not Found</div>} />
+        </Switch>
       </div>
     );
   }
